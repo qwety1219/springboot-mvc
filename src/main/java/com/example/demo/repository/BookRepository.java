272c0a2216c52cc7ff87608ab288c95a693ebdfe
kanco <kanco@MSI.mshome.net>
2026-03-19 21:24:20 +0800

    fifteenth work

--- src/main/java/com/example/demo/repository/BookRepository.java
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -1,5 +1,20 @@
 package com.example.demo.repository;
 
-public class BookRepository {
+import java.util.List;
+import java.util.Optional;
+
+import com.example.demo.model.Book;
+
+public interface BookRepository {
+	//查找書籍
+	List<Book> findAllBooks();
+	//查找指定書籍
+	Optional<Book> getBookById(Integer id);
+	//新增書籍
+	boolean addBook(Book book);
+	//修改書籍
+	boolean updateBook(Integer id,Book book);
+	//刪除書籍
+	boolean deleteBook(Integer id);
 
 }
--- src/main/java/com/example/demo/repository/BookRepositoryImpl.java
+++ src/main/java/com/example/demo/repository/BookRepositoryImpl.java
@@ -1,5 +1,70 @@
 package com.example.demo.repository;
 
-public class BookRepositoryImpl {
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.model.Book;
+//專門負責資料存取的元件,Spring會自動建立並管理該物件
+@Repository
+public abstract  class BookRepositoryImpl implements BookRepository{
+	//InMemory版
+	private List<Book> books=new CopyOnWriteArrayList<>();
+	//初始資料有四本書
+	{
+		books.add(new Book(1,"小叮噹",12.5,20,true));
+		books.add(new Book(2,"老夫子",10.5,30,true));
+		books.add(new Book(3,"好小子",9.5,40,true));
+		books.add(new Book(4,"新樂園",14.5,50,false));
+	}
+	@Override
+	public List<Book> findAllBooks() {
+		return books;
+	}
+
+	@Override
+	public Optional<Book> getBookById(Integer id) {
+		return books.stream().filter(book->book.getId().equals(id)).findFirst();
+	}
+
+	@Override
+	public boolean addBook(Book book) {
+		//建立newId
+		OptionalInt optMaxId=books.stream().mapToInt(Book::getId).max();
+		int newId=optMaxId.isEmpty()?1:optMaxId.getAsInt()+1;
+		//將newId設定給book
+		book.setId(newId);
+		return books.add(book);
+	}
+
+	@Override
+	public boolean updateBook(Integer id, Book book) {
+		//透過id找到要修改的書
+		Optional<Book> optBook=getBookById(id);
+		if(optBook.isEmpty()) {
+			return false;
+		}
+		//得到要修改的書
+		Book orginalBook=optBook.get();
+		orginalBook.setAmount(book.getAmount());
+		orginalBook.setName(book.getName());
+		orginalBook.setPrice(book.getPrice());
+		orginalBook.setPub(book.getPub());
+		return true;
+	}
+
+	@Override
+	public boolean deleteBook(Integer id) {
+		//透過id找到要刪除的書籍
+		Optional<Book> optBook=getBookById(id);
+		if(optBook.isEmpty()) {
+			return false;
+		}
+		//移除書籍
+		return books.remove(optBook.get());
+	}
 
 }
