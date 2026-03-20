7c6262923e1eff4e2220db10ed2bddde8609499c
kanco <kanco@MSI.mshome.net>
2026-03-20 12:46:12 +0800

    eighteenth work

--- src/main/java/com/example/demo/controller/BookController.java
+++ src/main/java/com/example/demo/controller/BookController.java
@@ -1,8 +1,17 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Book;
+import com.example.demo.response.ApiResponse;
+import com.example.demo.service.BookService;
 
 /**
  * BookController
@@ -81,6 +90,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/book")
 public class BookController {
-	
+	@Autowired
+	@Qualifier("bookServiceImpl")//若該interface只有一個實現類別,則@Qualifier可以省略
+	private BookService bookService;
+	public ResponseEntity<ApiResponse<List<Book>>> findAllBooks() {
+		List<Book> books=bookService.findAllBooks();
+		if(books.size()==0) {
+			return ResponseEntity.badRequest().body(ApiResponse.fail("查無任何書籍"));
+		}
+		return ResponseEntity.ok(ApiResponse.success("查詢成功", books));
+	}
 
 }
