2c069e2b629ca850f933b77829a5257756478036
kanco <kanco@MSI.mshome.net>
2026-03-25 21:07:21 +0800

    thirty-third

--- src/main/resources/static/js/book.js
+++ src/main/resources/static/js/book.js
@@ -82,7 +82,8 @@ async function addBook(){
 		const result=await handleResponse(response);
 		console.log(result);
 		showMessage(result.message||"新增成功","success");
-		bookForm.reset();
+		bookForm.reset();//清空表單
+		findAllBooks();//重新查詢所有書籍
 	}catch(error){
 		showMessage(error.message||"error");
 	}
