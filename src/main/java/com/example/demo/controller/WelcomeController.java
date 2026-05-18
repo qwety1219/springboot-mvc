204eb07cf367f76482cad604e1a029e0468385db
kanco <kanco@MSI.mshome.net>
2026-05-18 19:35:41 +0800

    forty-eighth

---
+++ src/main/java/com/example/demo/controller/WelcomeController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+
+@Controller
+public class WelcomeController {
+	@GetMapping("/welcome")
+	public String welcome(Model model) {
+		//model裡面放的是傳給jsp的資料
+		model.addAttribute("name","阿山哥");
+		model.addAttribute("now",new Date());
+		return "welcome";
+	}
+}
